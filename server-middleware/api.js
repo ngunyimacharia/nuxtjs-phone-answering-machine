636c19886a0645ffe8575eeddfaf8725fdb71b8b
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-02-04 18:21:57 +0200

    Phone call recording complete

---
+++ server-middleware/api.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const app = require('express')();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: process.env.NUXT_ENV_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
+
+app.all('/callback', async (request, response) => {
+
+    const body = request.body;
+
+    console.log(body);
+
+    if (body.CallStatus !== "completed") {
+        const twiml = new VoiceResponse();
+        twiml.say('Hello. Please leave a message after the beep.');
+        twiml.record();
+        twiml.hangup();
+        response.type('text/xml');
+        return response.send(twiml.toString());
+    }
+
+    const uploaded = await cloudinary.v2.uploader.upload(
+        body.RecordingUrl,
+        {
+            resource_type: "raw",
+            folder: "nuxtjs-phone-answering-machine",
+            context: `From=${body.From}|FromCountry=${body.FromCountry}|FromCity=${body.FromCity}`
+        },
+        function (error, result) { console.log(result, error) });
+
+    return response.json({ uploaded });
+});
+
+module.exports = app
